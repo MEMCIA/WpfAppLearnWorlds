b7e55c7c89023ebaf323f00b44871bfb7d03f5fe
MEMCIA <84227329+MEMCIA@users.noreply.github.com>
2022-06-25 15:00:06 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, string> words = new Dictionary<string, string> { { "uroczy", "carino" },{"kot", "gatto" }, { "dobranoc", "buonanotte"} };
+        int numberOfWord = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var window = new Window1();
+            window.ShowDialog();
+
+        }
+
+        private void ButtonAddAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(Answer.Text))
+            {
+                //WordInItalian = words.Values
+            }
+        }
+    }
+}
